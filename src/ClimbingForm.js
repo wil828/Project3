a45f62433724341c8ec74fa89e98be712f20b194
william <williamw8828@gmail.com>
2022-03-17 02:56:15 -0400

    fixed cosmetics

--- src/ClimbingForm.js
+++ src/ClimbingForm.js
@@ -36,9 +36,9 @@ function ClimbingForm( props ) {
         props.submitClimbingForm(event, dateValue, typeOfClimbValue, climbingGradeValue, climbingFinishValue);
     }
 
-    const climbingBoulder = ["VB", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13"];
+    const climbingBoulder = ["VB", "V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13"];
 
-    const climbingTopRope = ["5.1", "5.2", "5.3", "5.4", "5.5", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d", "5.14a", "5.14b", "5.14c", "5.14d"];
+    const climbingTopRope = ["5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d", "5.14a", "5.14b", "5.14c", "5.14d"];
     
 
 
